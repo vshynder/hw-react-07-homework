92a70f8f13397e7af8583eb0b4f49161ecafba51
Vlad Shynder <ShynderVlad@MacBook-Pro-Vlad.local>
2020-09-09 21:17:17 +0300

    Updates

---
+++ precache-manifest.81e26500599b32406f01e421a38ae4f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87ddcfd7ca716b3c5b0b00b9634b894c",
+    "url": "/hw-react-07-phonebook/index.html"
+  },
+  {
+    "revision": "69ec7ab9d2f24e33067f",
+    "url": "/hw-react-07-phonebook/static/css/main.cdcb6b6d.chunk.css"
+  },
+  {
+    "revision": "756d79c0955536386bb2",
+    "url": "/hw-react-07-phonebook/static/js/2.c538f662.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hw-react-07-phonebook/static/js/2.c538f662.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69ec7ab9d2f24e33067f",
+    "url": "/hw-react-07-phonebook/static/js/main.c8ce02b1.chunk.js"
+  },
+  {
+    "revision": "1455df719b09bced7119",
+    "url": "/hw-react-07-phonebook/static/js/runtime-main.e29d4660.js"
+  }
+]);
